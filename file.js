e8db13743d2a273e8d2637485a1f62c505fdda44
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2022-02-14 22:44:20 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,230 @@
+const targetNumbers = [];
+const allNumbers5Digit=[];
+const allPossibleNumbers=[];
+var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const NUMBER_LENGTH = 5
+const FLIP_ANIMATION_DURATION = 500
+const DANCE_ANIMATION_DURATION = 500
+const keyboard = document.querySelector("[data-keyboard]")
+const alertContainer = document.querySelector("[data-alert-container]")
+const guessGrid = document.querySelector("[data-guess-grid]")
+
+
+var arr = generatePermutations(digits, 5);
+arr.forEach(e => allPossibleNumbers.push(e.join('')));
+
+const targetNumber = allPossibleNumbers[Math.floor(Math.random()*30240)]
+
+
+startInteraction()
+generatePossibleNumbers()
+
+function generatePermutations(list, size) {
+  if (size > list.length) return [];
+  else if (size == 1) return list.map(d => [d]);
+  return list.flatMap(d =>
+    generatePermutations(
+      list.filter(a => a !== d),
+      size - 1
+    ).map(item => [d, ...item])
+  );
+}
+
+
+
+function generatePossibleNumbers(){
+  for (var i=10000; i<100000;i++){
+    var stringNumber=i.toString()
+    allNumbers5Digit.push(stringNumber)
+  }
+
+}
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick)
+  document.addEventListener("keydown", handleKeyPress)
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick)
+  document.removeEventListener("keydown", handleKeyPress)
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key)
+    return
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess()
+    return
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    deleteKey()
+    return
+  }
+
+  if (e.target.matches("[data-info]")) {
+    showAlert("Guess the 5-digit number.                           You'll have 5 attempts.The numbers are not repeated.\
+         🟩=CORRECT | 🟨=WRONG POSITION | ⬛=WRONG ", duration=4000)
+    return
+  }
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess()
+    return
+  }
+
+  if (e.key === "DEL" || e.key === "Backspace") {
+    deleteKey()
+    return
+  }
+
+  if (e.key.match(/^[0-9]$/)) {
+    pressKey(e.key)
+    return
+  }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles()
+  if (activeTiles.length >= NUMBER_LENGTH) return
+  const nextTile = guessGrid.querySelector(":not([data-letter])")
+  nextTile.dataset.letter = key.toLowerCase()
+  nextTile.textContent = key
+  nextTile.dataset.state = "active"
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles()
+  const lastTile = activeTiles[activeTiles.length - 1]
+  if (lastTile == null) return
+  lastTile.textContent = ""
+  delete lastTile.dataset.state
+  delete lastTile.dataset.letter
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()]
+  if (activeTiles.length !== NUMBER_LENGTH) {
+    showAlert("Not enough numbers")
+    shakeTiles(activeTiles)
+    return
+  }
+
+  const guess = activeTiles.reduce((word, tile) => {
+    return word + tile.dataset.letter
+  }, "")
+
+  if (!allNumbers5Digit.includes(guess)) {
+    showAlert("Not in a 5-digit number")
+    shakeTiles(activeTiles)
+    return
+  }
+
+  stopInteraction()
+  activeTiles.forEach((...params) => flipTile(...params, guess))
+}
+
+function flipTile(tile, index, array, guess) {
+  const letter = tile.dataset.letter
+  const key = keyboard.querySelector(`[data-key="${letter}"i]`)
+  setTimeout(() => {
+    tile.classList.add("flip")
+  }, (index * FLIP_ANIMATION_DURATION) / 2)
+
+  tile.addEventListener(
+    "transitionend",
+    () => {
+      tile.classList.remove("flip")
+      if (targetNumber[index] === letter) {
+        tile.dataset.state = "correct"
+        key.classList.add("correct")
+      } else if (targetNumber.includes(letter)) {
+        tile.dataset.state = "wrong-location"
+        key.classList.add("wrong-location")
+      } else {
+        tile.dataset.state = "wrong"
+        key.classList.add("wrong")
+      }
+
+      if (index === array.length - 1) {
+        tile.addEventListener(
+          "transitionend",
+          () => {
+            startInteraction()
+            checkWinLose(guess, array)
+          },
+          { once: true }
+        )
+      }
+    },
+    { once: true }
+  )
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]')
+}
+
+function showAlert(message, duration = 1000) {
+  const alert = document.createElement("div")
+  alert.textContent = message
+  alert.classList.add("alert")
+  alertContainer.prepend(alert)
+  if (duration == null) return
+
+  setTimeout(() => {
+    alert.classList.add("hide")
+    alert.addEventListener("transitionend", () => {
+      alert.remove()
+    })
+  }, duration)
+}
+
+function shakeTiles(tiles) {
+  tiles.forEach(tile => {
+    tile.classList.add("shake")
+    tile.addEventListener(
+      "animationend",
+      () => {
+        tile.classList.remove("shake")
+      },
+      { once: true }
+    )
+  })
+}
+
+function checkWinLose(guess, tiles) {
+  if (guess === targetNumber) {
+    showAlert("You Win", 5000)
+    danceTiles(tiles)
+    stopInteraction()
+    return
+  }
+
+  const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])")
+  if (remainingTiles.length === 0) {
+    showAlert(targetNumber.toUpperCase(), null)
+    stopInteraction()
+  }
+}
+
+function danceTiles(tiles) {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("dance")
+      tile.addEventListener(
+        "animationend",
+        () => {
+          tile.classList.remove("dance")
+        },
+        { once: true }
+      )
+    }, (index * DANCE_ANIMATION_DURATION) / 5)
+  })
+}
